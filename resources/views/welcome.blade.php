58cbb6d4942f8ab8edeb01c1e67be7943181c194
manuel <manuelpuertogutierrez@.com>
2021-01-03 13:18:17 +0100

    cambios login y registro

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -34,23 +34,36 @@
                     <li class="nav-item">
                         <a class="nav-link" href="{{ url('/blog') }}">Blog</a>
                     </li>
-        
-                    @if (Route::has('login'))
-                        @auth
+
+                    @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                        @if (Route::has('register'))
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ url('/home') }}">Home</a>
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Registro') }}</a>
                         </li>
+                    @endif
                         @else
-                            <li>
-                                <a class="nav-link" href="{{ route('login') }}">Login</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li>
-                                    <a class="nav-link" href="{{ route('register') }}">Registro</a>
-                                </li>
-                            @endif
-                        @endauth
-                        @endif
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                    onclick="event.preventDefault();
+                                                    document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
+                                </a>
+
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest
+
                 </ul>
             </div>
         </nav>
--- routes/web.php
+++ routes/web.php
@@ -18,13 +18,13 @@ Route::get('/', function () {
 Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/categories', 'CategoriesController@getAll')->name('categories');
-Route::get('/newCategoryForm', 'CategoriesController@newCategoryForm')->name('categoryCreate');
-Route::post('/newCategory', 'CategoriesController@introduceCategory')->name('newCategory');
-Route::get('/newProductForm/{categoryId}', 'ProductsController@newProductForm');
-Route::post('/newProduct', 'ProductsController@introduceProduct')->name('newProduct');
+Route::get('/newCategoryForm', 'CategoriesController@newCategoryForm')->name('categoryCreate')->middleware('auth');
+Route::post('/newCategory', 'CategoriesController@introduceCategory')->name('newCategory')->middleware('auth');
+Route::get('/newProductForm/{categoryId}', 'ProductsController@newProductForm')->middleware('auth');
+Route::post('/newProduct', 'ProductsController@introduceProduct')->name('newProduct')->middleware('auth');
 Route::get('/showcategory/{categoryId}', 'CategoriesController@getShow');
 Route::get('/allProducts', 'ProductsController@getAllProducts'); 
 Route::get('/blog', 'PostsController@getAllPosts'); 
-Route::get('/newBlogForm', 'PostsController@newPostForm'); 
+Route::get('/newBlogForm', 'PostsController@newPostForm')->middleware('auth'); 
 Route::post('/newPost', 'PostsController@introducePost')->name('newPost');
 
